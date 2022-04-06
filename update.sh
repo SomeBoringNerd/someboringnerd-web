46678796f551e06970689080cf0c8037fbaa97c3
A Boring Nerd <pro.corentynsauvage@gmail.com>
2022-04-06 09:37:51 +0200

    initial commit

---
+++ update.sh
@@ -0,0 +1,11 @@
+# this script will update the website by moving 
+# the content of html to the root of the website,
+# over-writting older file.
+
+# you should execute that as root
+
+cp html/ /var/www/html/ -rf
+
+# required by Nginx for file upload for exemple.
+# without it, stuff might break.
+chown -R www-data /var/www/html/
