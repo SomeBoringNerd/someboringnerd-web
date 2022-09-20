1a6c849f72f82fc4535125a75fbcb978f83df540
Some Boring Nerd <dev.someboringnerd@protonmail.com>
2022-09-20 10:14:44 +0200

    Update

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname + '/static')));
+
+app.listen(1234, () => {
+    console.log("http://localhost:1234")
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'html', 'index.html'))
+})
