13e4b993733ec6ae4cdbc75522969c17ec1bc3e0
SomeBoringNerd <dev.someboringnerd@protonmail.com>
2023-03-28 16:45:45 +0200

    removed tailwind, updated update script

--- update.sh
+++ update.sh
@@ -1,17 +1,6 @@
-# this script will update the website by moving 
-# the content of html to the root of the website,
-# over-writting older file.
-# if the entirety of your website is static, 
-# you can remove that line to delete unused, older files
-
-# sudo rm -rf /var/www/html/*
+git reset --hard
 
 # pull from github
 git pull
 
-# you have execute that as root
-cp html/* /var/www/html/ -rf
-
-# required by Nginx for file upload for exemple.
-# without it, stuff might break.
-chown -R www-data /var/www/html/
+systemctl restart website.service
