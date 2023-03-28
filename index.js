ea141218a7453b0dd1fffbdefe53f9eb257ca48e
SomeBoringNerd <dev.someboringnerd@protonmail.com>
2023-03-28 12:30:00 +0200

    Remade website once again

--- index.js
+++ index.js
@@ -3,23 +3,53 @@ const path = require('path')
 
 const app = express()
 
+const projects = require("./static/projects.json")
+
+// EJS thingy
+app.set('view engine','ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/pages');
+
 app.use(express.static(path.join(__dirname + '/static')));
 
 app.listen(1234, () => {
     console.log("http://localhost:1234")
 })
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static', 'html', 'index.html'))
+app.get('/', (req, res) => 
+{
+    let birth_day = new Date('2/1/2004');
+    let today = new Date();
+
+    let diffYear = (Math.abs(birth_day - today) / (1000 * 60 * 60 * 24 * 365.25)).toFixed(0);
+
+    res.render('index', {year: diffYear})
 })
 
-app.get('/waifuhax', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static', 'html', 'WaifuHax.html'))
+app.get('/wh', (req, res) => {
+    res.render('wh')
+    //res.sendFile(path.join(__dirname, 'static', 'html', 'WaifuHax.html'))
 })
 
 app.get('/ttd', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static', 'html', 'ttd.html'))
+    res.render('ttd')
 })
+
+app.get('/projects', (req, res) => 
+{
+    res.render('projects', {project_obj : projects})
+})
+
+app.get('/api/project', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'projects.json'))
+})
+
+app.use(function(req, res, next) {
+    res.status(404).render('404');
+});
+
+/*
+
 
 app.get('/pbm', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'html', 'pbm.html'))
@@ -31,4 +61,4 @@ app.get('/sitemap.xml', (req, res) => {
 
 app.get('/Robots.txt', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'html', 'Robots.txt'))
-})
+})*/
