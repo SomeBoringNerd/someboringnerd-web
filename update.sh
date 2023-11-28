8a5801b5042ad9f20433669e3ff31d0da692d635
SomeBoringNerd <someboringnerd@gmail.com>
2023-11-28 17:25:02 +0100

    Test updated script

--- update.sh
+++ update.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+previous="$(git rev-parse --short HEAD)"
+
 # reset any temp changes I could have made in prod
 git reset --hard
 
 # get my changes from github
 git pull
 
-# actualize my node_modules folder
-npm i
+now="$(git rev-parse --short HEAD)"
 
-# build the project
-npm run build
+# we should restart the server
+if [ "$previous" != "$now" ]; then
+	# actualize my node_modules folder
+	npm i
 
-# restart my website
-systemctl restart website.service
+	# build the project
+	npm run build
 
+	# restart my website
+	systemctl restart website.service
+else
+	echo "nothing changed, no need to restart the server"
+fi
 chmod +x update.sh
